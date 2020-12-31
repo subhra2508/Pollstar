7ef42caf032b64065db8d31a04876be8b536fc9b
subhrajyoti sankar sau <subhrajyotisankarsau@gmail.com>
2021-01-01 01:22:11 +0530

    Update settings.py

--- pollstar/settings.py
+++ pollstar/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6_=k5xje-uz90ja=_gqrb-v0+vqdha@*@!s%*()vgw)+&%8cij'
+ 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
 ALLOWED_HOSTS = []
 
